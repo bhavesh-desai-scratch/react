c7b2dd369a9a4dd16aabe52b0b7d2e4bbd52511a
BHAVESH DESAI <bhaveshdesai@BHAVESHs-MacBook-Pro.local>
2015-10-20 22:39:41 +0100

    initial commit

---
+++ src/components/Master.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Master extends Component {
+    render() {
+        return <div>ok</div>;
+    }
+}
+
+export default Master
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+//import ReactDom from 'react-dom';
+import Master from './components/Master';
+
+React.render(<Master />, document.getElementById('root'));
